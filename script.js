4bae4c93c94e5767e3180d97049c43bb1b3d8400
eduardoliveirap <117646670+eduardoliveirap@users.noreply.github.com>
2023-01-11 09:59:00 -0300

    update

---
+++ script.js
@@ -0,0 +1,31 @@
+// document.getElementById("container").innerHTML = "conteúdo";
+
+let container = document.getElementById("container");
+
+// let macas = 2;
+// let banana = 3;
+// let soma = macasbanana;
+
+// container.innerHTML = soma;
+
+// let texto = "Meu nome é";
+// let nome  = " Gleidson teixeira";
+
+// let soma = texto + nome;
+
+// container.innerHTML = soma;
+
+// const NASCIMENTO = "07/09/90";
+// let nome = "Gleidson Teixeira";
+
+// container.innerHTML = nome+ NASCIMENTO;
+
+function somar ()
+{
+    let v1 = Number(valor1.value);
+    let v2 = Number(valor2.value);
+    document.getElementById("resultado").innerHTML = v1 + v2;
+}
+
+
+
